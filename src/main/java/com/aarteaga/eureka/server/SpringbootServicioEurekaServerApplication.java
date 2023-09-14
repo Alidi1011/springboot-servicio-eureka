7e30c433455d44e9b50b718d60d415f9028cec7d
Alisson <arteaga10alisson@gmail.com>
2023-09-14 10:41:21 -0500

    add first commit create project

---
+++ src/main/java/com/aarteaga/eureka/server/SpringbootServicioEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.aarteaga.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aarteaga/eureka/server/SpringbootServicioEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarteaga.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
